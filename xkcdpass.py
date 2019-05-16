72dddb032b39cc6923e7d7bb568c2108d8d6729d
Carl Harris <elgoogemail2007@gmail.com>
2019-05-16 04:12:23 -0400

    added --number, --special and --wrap options

--- xkcdpass.py
+++ xkcdpass.py
@@ -1,5 +1,6 @@
 import argparse
 import functools
+import random
 import secrets
 import string
 
@@ -64,6 +65,15 @@ class XKCDPass:
         password = self.sep.join(secrets.choice(self.words) for i in range(self.nwords))
         return password
 
+wordwrappers = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>',
+    '"': '"',
+}
+wordwrappers.update([(v, k) for k, v in wordwrappers.items()])
+
 def main(argv=None):
     """
     Generate xkcd passwords.
@@ -71,9 +81,50 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description=main.__doc__, prog='xkcdpass')
     parser.add_argument('-n', '--num', type=int, default=10,
                         help='Number of passwords to generate. Default: %(default)s')
+    parser.add_argument('-N', '--number', action='store_true',
+                        help='Randomly change a letter to a number.')
+    parser.add_argument('-S', '--special', action='store_true',
+                        help='Randomly insert a special character.')
+    parser.add_argument('-W', '--wrap', action='store_true',
+                        help='When the randomly selected special character is a'
+                             ' wrapper, wrap the randomly selected word with'
+                             ' it. Requires --special.')
     args = parser.parse_args(argv)
+
+    if args.wrap and not args.special:
+        parser.error('option --wrap requires --special')
+
     xkcdpass = XKCDPass()
-    passwords = (xkcdpass.password() for _ in range(args.num))
+
+    passwords = []
+    for _ in range(args.num):
+        password = xkcdpass.password()
+
+        # TODO: put this up in the XKCDPass class
+        index = None
+        if args.number:
+            words = password.split()
+            index = random.randrange(len(words))
+            words[index] += str(random.randrange(10))
+            password = xkcdpass.sep.join(words)
+
+        if args.special:
+            words = password.split()
+            if index is not None:
+                while True:
+                    newindex = random.randrange(len(words))
+                    if newindex != index:
+                        break
+            char = secrets.choice(string.punctuation)
+            if char in wordwrappers:
+                opp = wordwrappers[char]
+                l, r = sorted([char, opp])
+                words[newindex] = l + words[newindex] + r
+            else:
+                words[newindex] += char
+            password = xkcdpass.sep.join(words)
+
+        passwords.append(password)
     print('\n'.join(passwords))
 
 if __name__ == '__main__':
