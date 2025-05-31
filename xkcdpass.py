011c4702d4b9646674cdebcbb232237c9b86778a
Carl Harris <hitbox@ryzen.com>
2025-05-31 12:47:51 -0400

    Starting constraint and mutator idea.

--- xkcdpass.py
+++ xkcdpass.py
@@ -4,6 +4,8 @@ import os
 import random
 import string
 
+from abc import ABC
+from abc import abstractmethod
 from collections import ChainMap
 from configparser import ConfigParser
 from types import SimpleNamespace
@@ -17,6 +19,110 @@ SPECIALS = '!@#$%^&*'
 
 class XKCDPassError(Exception):
     pass
+
+
+class ValidateError(Exception):
+    pass
+
+
+class Validate(ABC):
+
+    @abstractmethod
+    def __call__(self, string):
+        pass
+
+
+class Contains(Validate):
+
+    def __init__(self, substrings):
+        self.substrings = substrings
+
+    def __call__(self, string):
+        for substring in self.substrings:
+            if substring in string:
+                break
+        else:
+            raise ValidateError(f'{string} does not contain any of {self.substrings}')
+
+
+class Excludes(Validate):
+
+    def __init__(self, substrings):
+        self.substrings = substrings
+
+    def __call__(self, string):
+        for substring in self.substrings:
+            if substring in string:
+                raise ValidateError(f'{string} contains excluded {substring}')
+
+
+class StartsWith(Validate):
+
+    def __init__(self, substring):
+        self.substring = substring
+
+    def __call__(self, string):
+        for substring in self.substrings:
+            if string.startswith(substring):
+                break
+        else:
+            raise ValidateError(f'{string} does not start with any of {self.substrings}')
+
+
+class MinimumLength(Validate):
+
+    def __init__(self, length):
+        self.length = length
+
+    def __call__(self, string):
+        if len(string) < self.length:
+            raise ValidateError(f'{string} is less than {self.length}')
+
+
+class Mutator(ABC):
+
+    @abstractmethod
+    def __call__(self, strings):
+        pass
+
+
+class AddDigit(Mutator):
+
+    def __init__(self, whitespace, avoid_first=True):
+        self.whitespace = whitespace
+        self.avoid_first = avoid_first
+
+    def __call__(self, strings):
+        assert isinstance(string, list)
+        assert all(isinstance(item, str) for item in strings)
+        result = strings.copy()
+        # TODO
+
+        indexes = list(range(len(strings)))
+        index = random.choice(indexes)
+        # Want to make a new list and modify it and return it.
+        # A list of string is great because string are immutable. Just copy the list.
+
+        part = random.choice(strings)
+        insert_start = random.choice([True, False])
+        if insert_start and self.avoid_first and strings.index(part) == 0:
+            insert_start = False
+        if insert_start:
+            pass
+
+
+class SubstituteDigit(Mutator):
+
+    def __init__(self, replaceable):
+        """
+        :param replaceable:
+            Iterable of characters it is permitted to replace with digits. If a
+            mapping, then the values are an iterable of replacement characters.
+        """
+        self.replaceable = replaceable
+
+    def __call__(self, string):
+        pass
 
 
 def random_insert(word, char):
